041d585eefa9beca0b8d5644857abb3102eede47
shiva shankar <sivas1475@gmail.com>
2022-12-28 05:04:40 +0530

    JENKINS

--- vars.tf
+++ vars.tf
@@ -7,8 +7,6 @@ variable "AZ" {}
 variable "ENGINE" {}
 variable "ENGINE_VERSION" {}
 variable "NODE_TYPE" {}
-variable "ENV" {}
-variable "COMPONENT" {}
 variable "NODE_COUNT" {}
 variable "FAMILY" {}
 variable "SKIP_FINAL_SNAPSHOT" {}

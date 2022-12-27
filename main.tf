f271bc7c028a72ed9db82ebb94016c9120c94ece
shiva shankar <sivas1475@gmail.com>
2022-12-28 05:03:27 +0530

    JENKINS

--- main.tf
+++ main.tf
@@ -6,3 +6,16 @@ module "vpc" {
   SUBNET_CIDR = var.SUBNET_CIDR
   AZ = var.AZ
 }
+
+module "documentdb" {
+  depends_on          = [module.vpc]
+  source              = "git::https://github.com/raghudevopsb64/tf-module-documentdb.git"
+  ENGINE              = var.ENGINE
+  ENGINE_VERSION      = var.ENGINE_VERSION
+  NODE_TYPE           = var.NODE_TYPE
+  ENV                 = var.ENV
+  COMPONENT           = var.COMPONENT
+  NODE_COUNT          = var.NODE_COUNT
+  FAMILY              = var.FAMILY
+  SKIP_FINAL_SNAPSHOT = var.SKIP_FINAL_SNAPSHOT
+}
